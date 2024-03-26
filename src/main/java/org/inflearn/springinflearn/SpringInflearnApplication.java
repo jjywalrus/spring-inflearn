c9c36caf7e619950bd111057f43e6376b63726dc
jjywalrus <jjywalrus@gmail.com>
2024-03-26 11:29:43 +0900

    first commit

---
+++ src/main/java/org/inflearn/springinflearn/SpringInflearnApplication.java
@@ -0,0 +1,13 @@
+package org.inflearn.springinflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/inflearn/springinflearn/SpringInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inflearn.springinflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
